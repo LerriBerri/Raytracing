c553bca0167b4a2a27115a28a32538a1341a1994
LerriBerri <luissagerer@hotmail.com>
2022-11-20 13:13:01 +0100

    RT-4: Created Mesh class with constructor

---
+++ src/mesh/Mesh.cs
@@ -0,0 +1,11 @@
+namespace Raytracing
+{
+    class Mesh
+    {
+        Triangle[] triangles;
+
+        public Mesh(Triangle[] triangles) {
+            this.triangles = triangles;
+        }
+    }
+}
