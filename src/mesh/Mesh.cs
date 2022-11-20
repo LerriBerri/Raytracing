4b0f66bff0489c6bb8b837cd5cb9e5aa1f35ff30
LerriBerri <luissagerer@hotmail.com>
2022-11-20 13:24:47 +0100

    RT-4: Added GetNearestIntersection method

--- src/mesh/Mesh.cs
+++ src/mesh/Mesh.cs
@@ -4,8 +4,33 @@ namespace Raytracing
     {
         Triangle[] triangles;
 
-        public Mesh(Triangle[] triangles) {
+        public Mesh(Triangle[] triangles)
+        {
             this.triangles = triangles;
+        }
+
+        /**
+        <summary>
+        Determines the triangle whose intersection point with the line is the closest to l0.
+        If there is no intersection with any triangle the return value is null.
+        </summary>
+        */
+        public Triangle GetNearesIntersection(Vector3 l0, Vector3 l1)
+        {
+            double smallestDistance = -1;
+            Triangle nearestTriangle = null;
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                double d = triangles[i].GetIntersection(l0, l1);
+                if(d == -1)
+                    continue;
+                if(d == -1 || d < smallestDistance)
+                {
+                    smallestDistance = d;
+                    nearestTriangle = triangles[i];
+                }
+            }
+            return nearestTriangle;
         }
     }
 }
