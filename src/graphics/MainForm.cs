2e17edfd97282f1263525601a3e4e6d950e82cb4
LerriBerri <luissagerer@hotmail.com>
2022-11-25 13:21:30 +0100

    RT-22: Implemented rendering on Form

--- src/graphics/MainForm.cs
+++ src/graphics/MainForm.cs
@@ -6,8 +6,11 @@ namespace Raytracing
         System.Windows.Forms.Timer graphicsTimer;
         Point res;
 
+        Camera cam;
+        Mesh cube;
+
         public MainForm()
-        {   
+        {
             res = new Point(400, 400);
 
             InitializeComponent();
@@ -20,18 +23,57 @@ namespace Raytracing
             graphicsTimer = new System.Windows.Forms.Timer();
             graphicsTimer.Interval = 1;
             graphicsTimer.Tick += GraphicsTimer_Tick;
+
+            cam = new Camera(res, new Vector3(-4, 0, 0), Math.PI/2);
+
+            cube = new Mesh(new Triangle[] {
+                //neg X side
+                new Triangle(new Vector3(-1, -1, -1), new Vector3(-1, -1, 1), new Vector3(-1, 1, -1)),
+                new Triangle(new Vector3(-1, 1, 1), new Vector3(-1, 1, -1), new Vector3(-1, -1, 1)),
+                //pos Y side
+                new Triangle(new Vector3(-1, 1, -1), new Vector3(-1, 1, 1), new Vector3(1, 1, -1)),
+                new Triangle(new Vector3(1, 1, 1), new Vector3(1, 1, -1), new Vector3(-1, 1, 1)),
+                //pos Z side
+                new Triangle(new Vector3(-1, -1, 1), new Vector3(1, -1, 1), new Vector3(-1, 1, 1)),
+                new Triangle(new Vector3(1, 1, 1), new Vector3(-1, 1, 1), new Vector3(1, -1 ,1)),
+                //pos X side
+                new Triangle(new Vector3(1, -1, -1), new Vector3(1, -1, 1), new Vector3(1, 1, -1)),
+                new Triangle(new Vector3(1, 1, 1), new Vector3(1, 1, -1), new Vector3(1, -1, 1)),
+                //neg Z side
+                new Triangle(new Vector3(1, -1, -1), new Vector3(-1, -1, -1), new Vector3(1, 1, -1)),
+                new Triangle(new Vector3(-1, 1, -1), new Vector3(1, 1, -1), new Vector3(-1, -1, -1)),
+                //neg Y side
+                new Triangle(new Vector3(-1, -1, -1), new Vector3(-1, -1, 1), new Vector3(1, -1, -1)),
+                new Triangle(new Vector3(1, -1, 1), new Vector3(1, -1, -1), new Vector3(-1, -1, 1)),
+            });
         }
 
-        private void MainForm_Load(object sender, EventArgs e) {
+        private void MainForm_Load(object sender, EventArgs e)
+        {
             graphicsTimer.Start();
         }
 
-        private void MainForm_Paint(object sender, PaintEventArgs e) {
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
             //Paint
             Graphics g = e.Graphics;
+            Brush br = new SolidBrush(Color.White);
+            for (int x = 0; x < res.X; x++)
+            {
+                for (int y = 0; y < res.Y; y++)
+                {
+                    Triangle triangle = cube.GetNearesIntersection(cam.pos, Vector3.Add(cam.pos, cam.GetPointingTo(x, y)));
+                    if (triangle != null)
+                        g.FillRectangle(br, x, y, 1, 1);
+                }
+            }
+
+            cube.RotateX(.1);
+            cube.RotateZ(.2);
         }
 
-        private void GraphicsTimer_Tick(object sender, EventArgs e) {
+        private void GraphicsTimer_Tick(object sender, EventArgs e)
+        {
             Invalidate();
         }
     }
