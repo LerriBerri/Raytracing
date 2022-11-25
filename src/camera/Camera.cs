632609e36d73f7d7daefe8e6949dbd5a427f276a
LerriBerri <luissagerer@hotmail.com>
2022-11-25 12:41:09 +0100

    RT-18: camera implementation with simplifications

--- src/camera/Camera.cs
+++ src/camera/Camera.cs
@@ -6,11 +6,10 @@ namespace Raytracing
         int resX;
         int resY;
 
-        double fOVX;
-        double fOVY;
+        public double fOVX;
+        public double fOVY;
 
         public Vector3 pos;
-        Vector3 dir;
 
         public Camera(Point res, Vector3 pos, Vector3 dir, double fOV)
         {
@@ -18,14 +17,18 @@ namespace Raytracing
             resY = res.Y;
 
             this.pos = pos;
-            this.dir = dir;
 
             this.fOVX = fOV;
             fOVY = fOVX * ((double) resX / resY);
         }
 
         public Vector3 GetPointingTo(double x, double y) {
-            
+            double angleZ = (-y / resY + .5) * fOVY;
+            double angleY = (x / resX - .5) * fOVX;
+
+            double cosY = Math.Cos(angleY);
+
+            return new Vector3(cosY, Math.Tan(angleZ) * cosY, Math.Sin(angleY));
         }
     }
 }
